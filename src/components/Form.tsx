222408919de089299d9fec3eb6ccb96f4468ce87
Meruyert Karim <meruyert.karim@nu.edu.kz>
2020-11-13 00:52:54 +0100

    Adding Inputfiels: Text-area and Password

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import './Form.css'
+
 
 export type FormDescription = {
     inputFields: InputField[];
@@ -23,7 +26,7 @@ export const Form: React.FC<{formDetails: FormDescription}> = (props) => {
     const details =  props.formDetails
     console.log(details)
     return (
-        <div>
+        <div className="simpleForm">
             <h1>
                 {details.title}
             </h1>
@@ -32,8 +35,24 @@ export const Form: React.FC<{formDetails: FormDescription}> = (props) => {
                     return <TextField label={inputField.placeholder}/>
                 }
                 // TODO add support for input type text-area
-
+                if (inputField.type === 'text-area') {
+                    return <TextareaAutosize
+                    rowsMin={20}
+                    aria-label="Here some text"
+                    placeholder="Enter your text"
+                    defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                    ut labore et dolore magna aliqua."
+                    />
+                }
                 // TODO add support for input type password
+                if (inputField.type === 'password') {
+                    return <TextField
+                    id="standard-password-input"
+                    label="Password"
+                    type="password"
+                    autoComplete="current-password"
+                    />
+                }
             }
             )}
             <button>
--- src/pages/ExampleForm.tsx
+++ src/pages/ExampleForm.tsx
@@ -1,12 +1,16 @@
 import { Form, FormDescription, InputField } from "../components/Form"
 import React, { Component } from 'react';
 
+
 export class ExampleForm extends Component <{}, {}> {
 
     render() {
         const textInputField: InputField = {type: "text", placeholder: "Your name", label: "Enter your name"}
         const textInputField2: InputField = {type: "text", placeholder: "Your first name", label: "Enter your name"}
-        const exampleForm: FormDescription = {inputFields: [textInputField, textInputField2], title: "Simple Form", buttonTitle: "Submit form"}
+        const textInputField3: InputField = {type: "text-area", placeholder: "", label: "Enter your name"}
+        const textInputField4: InputField = {type: "password", placeholder: "", label: "Enter your password"}
+        const exampleForm: FormDescription = {inputFields: [textInputField, textInputField2, textInputField3, textInputField4], title: "Simple Form", buttonTitle: "Submit form"}
+        
 
         return <div><Form formDetails={exampleForm}/></div>
     }
