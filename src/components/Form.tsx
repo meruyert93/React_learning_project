0f2122aa545d2865bc2ad9e40658c850b4734dca
Meruyert Karim <meruyert.karim@nu.edu.kz>
2020-11-13 14:26:14 +0100

    Data Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import TextareaAutosize from '@material-ui/core/TextareaAutosize';
-import './Form.css'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 
+
+  //Styling
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap',
+        },
+        textField: {
+            marginLeft: theme.spacing(1),
+            marginRight: theme.spacing(1),
+            width: 200,
+      },
+    }),
+);
 
 export type FormDescription = {
     inputFields: InputField[];
@@ -16,45 +31,69 @@ export type InputField = {
     placeholder: string;
 }
 
-type RadioInputField = {
+export type RadioInputField = {
     type: string; // text, phone, check-box, radio
     values: string[];
 }
 
+// export type DataField =  {
+//        type: Date;
+// }  
+
 export const Form: React.FC<{formDetails: FormDescription}> = (props) => {
 
-    const details =  props.formDetails
+    const details =  props.formDetails;
+    const classes = useStyles();
+
     console.log(details)
     return (
-        <div className="simpleForm">
-            <h1>
+        <div>
+            <h1 className="title">
                 {details.title}
             </h1>
-            {details.inputFields.map((inputField) => {     
-                if (inputField.type === "text") {
-                    return <TextField label={inputField.placeholder}/>
+            <div> 
+                
+                {details.inputFields.map((inputField) => {     
+                    if (inputField.type === "text") {
+                        return <TextField label={inputField.placeholder}/>
+                    }
+                    // TODO add support for input type text-area
+                    if (inputField.type === 'text-area') {
+                        return <TextareaAutosize
+                        rowsMin={20}
+                        aria-label="Here some text"
+                        placeholder="Enter your text"
+                        defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                        ut labore et dolore magna aliqua."
+                        />
+                    }
+                    // TODO add support for input type password
+                    if (inputField.type === 'password') {
+                        return <TextField
+                        id="standard-password-input"
+                        label="Password"
+                        type="password"
+                        defaultValue="2017-05-24"
+                        autoComplete="current-password"
+                        />
+                    }
                 }
-                // TODO add support for input type text-area
-                if (inputField.type === 'text-area') {
-                    return <TextareaAutosize
-                    rowsMin={20}
-                    aria-label="Here some text"
-                    placeholder="Enter your text"
-                    defaultValue="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua."
+                )}
+                <form className={classes.container} noValidate>
+                    <TextField
+                        id="date"
+                        label="Birthday"
+                        type="date"
+                        defaultValue="2017-05-24"
+                        className={classes.textField}
+                        InputLabelProps={{
+                        shrink: true,
+                        }}
                     />
-                }
-                // TODO add support for input type password
-                if (inputField.type === 'password') {
-                    return <TextField
-                    id="standard-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                    />
-                }
-            }
-            )}
+                </form>
+
+
+            </div>
             <button>
                 {details.buttonTitle}
             </button>
--- src/pages/ExampleForm.tsx
+++ src/pages/ExampleForm.tsx
@@ -1,5 +1,6 @@
-import { Form, FormDescription, InputField } from "../components/Form"
+import {Form, FormDescription, InputField } from "../components/Form"
 import React, { Component } from 'react';
+import '../css/main.css';
 
 
 export class ExampleForm extends Component <{}, {}> {
@@ -7,12 +8,19 @@ export class ExampleForm extends Component <{}, {}> {
     render() {
         const textInputField: InputField = {type: "text", placeholder: "Your name", label: "Enter your name"}
         const textInputField2: InputField = {type: "text", placeholder: "Your first name", label: "Enter your name"}
-        const textInputField3: InputField = {type: "text-area", placeholder: "", label: "Enter your name"}
-        const textInputField4: InputField = {type: "password", placeholder: "", label: "Enter your password"}
-        const exampleForm: FormDescription = {inputFields: [textInputField, textInputField2, textInputField3, textInputField4], title: "Simple Form", buttonTitle: "Submit form"}
+        // const textInputField3: InputField = {type: "text-area", placeholder: "", label: "Enter your name"}
+        // const textInputField4: InputField = {type: "password", placeholder: "", label: "Enter your password"}
+        const nameForm: FormDescription = {inputFields: [textInputField, textInputField2], title: "Sign Up", buttonTitle: "Submit form"}
+        
+        
+       
         
 
-        return <div><Form formDetails={exampleForm}/></div>
+        return  <div className="containerForm">
+                    <div>
+                        <Form formDetails={nameForm}/>
+                    </div>
+                </div>
     }
     
 }
